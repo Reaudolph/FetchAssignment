d602a33a7d434526bb3201eb56d410d53314087d
Reaudolph <hrishivik@gmail.com>
2023-10-26 20:27:56 -0500

    Initial Commit

---
+++ MealDBStuff/MealDBStuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealDBStuffApp.swift
+//  MealDBStuff
+//
+//  Created by Hrishikesh Vikram on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct MealDBStuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
